98bd2f98808705c91a19f428dfd6d962adb891b7
Victor Serhiienko <107862956+srg72@users.noreply.github.com>
2023-03-15 18:30:09 +0200

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+refs.stopBtn.setAttribute('disabled', true);
+
+let intervalId = null;
+
+function onStart() {
+  refs.stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', true);
+  changeColor();
+}
+
+function onStop() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+  clearInterval(intervalId);
+}
+
+function changeColor() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
